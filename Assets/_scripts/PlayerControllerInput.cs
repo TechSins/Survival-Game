7df28945ba7070bc669e4773afc275f4402dbcc4
Nathan Blair <nathan.t.blair@outlook.com>
2016-01-22 21:59:20 -0600

    Initial Commit -Simple capsule in empty environment -Working on code to move capsule using standard inputs (keyboard, maybe controller) -To be used as a case study

---
+++ Assets/_scripts/PlayerControllerInput.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerInput : MonoBehaviour {
+
+	const float sneakSpeed = 0.75f;
+	const float sprintSpeed = 1.25f;
+
+	bool isCrouch = false;
+	bool isSprint = false;
+
+	float modSpeed = 1.0f;
+
+    float horiMovement = 0.0f;
+    float vertMovement = 0.0f;
+
+	Quaternion playerRotation;
+
+    Vector3 movement;
+
+    void Update()
+    {
+		// Get player movement input
+		if (Input.GetButton ("Crouch")) {
+			isCrouch = true;
+			modSpeed = sneakSpeed;
+		} else if (Input.GetButton ("Sprint")) {
+			isSprint = true;
+			modSpeed = sprintSpeed;
+		} else {
+			isSprint = false;
+			isCrouch = false;
+			modSpeed = 1.0f;
+		}
+
+		horiMovement = Input.GetAxis ("Horizontal") * modSpeed;
+		vertMovement = Input.GetAxis ("Vertical") * modSpeed;
+
+		// Get player rotation input
+
+
+    }
+    
+    void FixedUpdate()
+    {
+		MoveRotatePlayer ();
+    }
+
+
+	void MoveRotatePlayer()
+	{
+		
+		transform.Translate (horiMovement, 
+			vertMovement, 0.0f);
+
+		transform.rotation = playerRotation;
+
+		// Implement animations for combinations of 
+		// sneaking and sprinting
+
+	}
+}
